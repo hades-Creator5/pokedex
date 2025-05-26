1c4de0450574477ae628f745c60fc556a14fa161
hades-Creator5 <qwikerdestiker@gmail.com>
2025-05-26 10:30:14 +0200

    Profiel

--- index.php
+++ index.php
@@ -13,7 +13,21 @@ include "function.php";
 </head>
 <body>
 <div class="container">
-
+    <div class="card" style="width: 18rem;">
+        <img src="img/abra.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Bodhi</h5>
+            <p class="card-text">Leeftijd 16</p>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="form-floating">
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+            <label for="floatingTextarea">Comments</label>
+        </div>
+    </div>
 </div>
 
 <script src="js/bootstrap.bundle.min.js"></script>
